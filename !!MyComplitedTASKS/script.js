1988eb6ea7508f8efd20c971f76ee661beba81d5
Vadim <triv@me.com>
2019-07-31 21:06:12 -0400

    practice added

--- !!MyComplitedTASKS/script.js
+++ !!MyComplitedTASKS/script.js
@@ -1,9 +1,28 @@
-let firstName = 'John';
+//**************
+// if- else
+
+/*let firstName = 'John';
 let civilStarus = 'single';
 
 if(civilStarus === 'married'){
     console.log(firstName + ' is married');
 } else {
     console.log(firstName + ' will hopefully marry soon');
+}
+
+ */
+
+// ****************
+//Boolean ligi
+
+let firstName  = 'John';
+let age = 16;
+
+if(age < 13){
+    console.log(firstName + ' is a boy');
+} else if (age >=13 && age < 20) {
+    console.log(firstName + ' is a teenager');
+} else {
+    console.log(firstName + ' is a men');
 }
 
