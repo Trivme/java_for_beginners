615e9de52997178132524481a59b9895102533f3
Vadim <triv@me.com>
2019-07-31 20:51:40 -0400

    First practice added

---
+++ !!MyComplitedTASKS/script.js
@@ -0,0 +1,9 @@
+let firstName = 'John';
+let civilStarus = 'single';
+
+if(civilStarus === 'married'){
+    console.log(firstName + ' is married');
+} else {
+    console.log(firstName + ' will hopefully marry soon');
+}
+
